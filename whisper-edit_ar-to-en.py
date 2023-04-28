faddaee72457b0055d975af70743ac009aa8ad36
ole <ole@shortcutoslo.no>
2023-04-28 16:53:54 +0200

    changes for the better

--- whisper-edit_ar-to-en.py
+++ whisper-edit_ar-to-en.py
@@ -2,8 +2,8 @@ import whisper
 from whisper.utils import get_writer
 
 def main():
-    model = whisper.load_model("base")
-    result = model.transcribe("/Users/ole/Downloads/tst1.wav", task = "translate")
+    model = whisper.load_model("medium", device="cpu")
+    result = model.transcribe("/Users/ole/Downloads/tst1.wav", task = "translate", verbose = "True", fp16 = False, language = "ar")
 
     srt_writer = get_writer("srt", "/Volumes/temp/whisper_srt")
     srt_writer(result, "tst1")
