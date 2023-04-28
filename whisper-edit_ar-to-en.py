8de2ba0918b59fb192ca6f54bc48d7cc53f24d62
ole <ole@shortcutoslo.no>
2023-04-28 16:54:11 +0200

    Changes for the better

--- whisper-edit_ar-to-en.py
+++ whisper-edit_ar-to-en.py
@@ -13,8 +13,3 @@ def main():
     print(result["text"])
 
 main()
-
-
-
-#with open("/Volumes/temp/whisper_srt/tst1.srt", "w", encoding="utf-8") as srt_file:
-#        write_srt(result["segments"], file=srt_file)
