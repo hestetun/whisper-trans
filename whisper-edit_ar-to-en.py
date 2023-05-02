aaa2f5a042644caa1de2cbcd92c5efa35b8ef374
ole <ole@shortcutoslo.no>
2023-05-02 16:02:35 +0200

    Added some basic input /w guidance.

--- whisper-edit_ar-to-en.py
+++ whisper-edit_ar-to-en.py
@@ -2,14 +2,16 @@ import whisper
 from whisper.utils import get_writer
 
 def main():
-    model = whisper.load_model("medium", device="cpu")
-    result = model.transcribe("/Volumes/temp/whisper_sample/arabic_tedtalk.wav", task = "translate", verbose = "True", fp16 = False, language = "ar")
+    filepath = input("What file do you want to transcribe?\n")
+    lang = input("What language do you want to transcribe to English? Input using ISO 639-1 codes. e.g Norwegian or no.\n")
+    print("Please wait...")
+
+    model = whisper.load_model("large-v2")
+    result = model.transcribe(filepath, task = "translate", verbose = "True", fp16 = False, language = lang, temperature=0)
 
     srt_writer = get_writer("srt", "/Volumes/temp/whisper_srt")
     srt_writer(result, "tst1")
 
-
-
-    print(result["text"])
+    print(f"Transcription done using OpenAI Whisper from {lang} to English. \nTime spent: ")
 
 main()
