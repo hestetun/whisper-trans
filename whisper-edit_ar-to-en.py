222a955550c6c009a781ad440769ad8eeb03e5c5
ole <ole@shortcutoslo.no>
2023-05-02 13:59:33 +0200

    updated sample-file

--- whisper-edit_ar-to-en.py
+++ whisper-edit_ar-to-en.py
@@ -3,7 +3,7 @@ from whisper.utils import get_writer
 
 def main():
     model = whisper.load_model("medium", device="cpu")
-    result = model.transcribe("/Users/ole/Downloads/tst1.wav", task = "translate", verbose = "True", fp16 = False, language = "ar")
+    result = model.transcribe("/Volumes/temp/whisper_sample/arabic_tedtalk.wav", task = "translate", verbose = "True", fp16 = False, language = "ar")
 
     srt_writer = get_writer("srt", "/Volumes/temp/whisper_srt")
     srt_writer(result, "tst1")
