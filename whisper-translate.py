ecc83fc2062e8eb96ef6fa9ddc45e9996d2f2e20
ole <ole@shortcutoslo.no>
2023-08-14 16:31:06 +0200

    Changed structure and proper naming

--- whisper-translate.py
+++ whisper-translate.py
@@ -5,20 +5,21 @@ from whisper.utils import get_writer
 
 def main():
     filepath = input("What file do you want to transcribe?\n")
+    lang = input("What language do you want to transcribe to English? Input using ISO 639-1 codes. e.g Norwegian or no.\n")
     output_dir = os.path.dirname(filepath)
     print("Please wait...")
 
     model = whisper.load_model("large-v2")
-    result = model.transcribe(filepath, verbose=True, fp16=False, language="Norwegian", patience=2, beam_size=5)
+    result = model.transcribe(filepath, task="translate", verbose=True, fp16=False, language=lang, patience=2, beam_size=5)
 
     # Generate output file name
     base_filename = os.path.splitext(os.path.basename(filepath))[0]
     timestamp = datetime.datetime.now().strftime("%y%m%d")
-    output_path = os.path.join(f"{base_filename}_{timestamp}")
+    output_path = os.path.join(f"{base_filename}_{lang}_{timestamp}")
 
     # Write output to SRT file
     srt_writer = get_writer("srt", output_dir)
     srt_writer(result, output_path)
-    print(f"Transcription done using OpenAI Whisper. Output file saved to {output_dir}.")
+    print(f"Transcription done using OpenAI Whisper from {lang} to English. Output file saved to {output_dir}.")
 
 main()
